55745a24bf5b7491ab6115ac0c338e0b41c0740e
shashwatjv <shashwat@pdx.edu>
2017-03-04 14:52:38 -0800

    change funcs to be automatic

--- design/L2CLINE.sv
+++ design/L2CLINE.sv
@@ -15,59 +15,46 @@ class L2CLINE;
    // Create a cache line of CLINE type
    CLINE myline;
 
-   int function chk_tag (input [L2_TAG_LENGTH-1:0] in_tag);
-      begin
-	 return (in_tag === myline.tag);
-      end
+   function automatic int chk_tag (input [L2_TAG_LENGTH-1:0] in_tag);
+      return (in_tag === this.myline.tag);
    endfunction // chk_tag
 
-   TYP_RU_NUM function get_ru_num ();
-      begin
-	 return myline.ru_num;
-      end
+   function automatic TYP_RU_NUM get_ru_num ();
+      return this.myline.ru_num;
    endfunction // get_ru_num
 
-   MESI_STATES function get_mesi_bits ();
-      begin
-	 return myline.mesi;
-      end
+   function automatic MESI_STATES get_mesi_bits ();
+      return this.myline.mesi;
    endfunction // get_mesi_bits
 
-   int function updt_ru_num(input TYP_RU_NUM);
+   function automatic int updt_ru_num(input TYP_RU_NUM in_ru);
       // return 0 if we successfully update else, -1 if we under/overflow the RU_NUM
-      begin
-	 // Incoming RU_NUM is the RU_NUM on which current read/write operation 
-	 //   is performed.
-	 // LRU = 0 , MRU = MAX-1
-	 // Algo -
-	 // If incoming RU_NUM is same as THIS line's num
-	 //     Make THIS line MOST RECENT
-	 // Else If incoming RU_NUM > THIS RU_NUM
-	 //     Decrement THIS RU_NUM by 1
-	 // Else
-	 //     Keep retain THIS RU_NUM
-	 // TODO :: 
-	 // add logic of updating the Recently Number
-      end
+
+      // Incoming RU_NUM is the RU_NUM on which current read/write operation 
+      //   is performed.
+      // LRU = 0 , MRU = MAX-1
+      // Algo -
+      // If incoming RU_NUM is same as THIS line's num
+      //     Make THIS line MOST RECENT
+      // Else If incoming RU_NUM > THIS RU_NUM
+      //     Decrement THIS RU_NUM by 1
+      // Else
+      //     Keep retain THIS RU_NUM
+      // TODO :: 
+      // add logic of updating the Recently Number
    endfunction // updt_ru_num
 
-   int function updt_mesi(input MESI_STATES);
+   function automatic int updt_mesi(input MESI_STATES in_mesi);
       // return 0 if successfully updated the mesi bits, -1 if catch erronous state change
-      begin
-	 // TODO ::
-      end
+      // TODO ::
    endfunction // updt_mesi
 
-   void function put_data(BYTE [L2_LINE_SZ-1:0] din);
-      begin
-	 myline.data = din;
-      end
+   function automatic void put_data(BYTE [L2_LINE_SZ-1:0] din);
+      this.myline.data = din;
    endfunction // put_data
 
-   BYTE [L2_LINE_SZ-1:0] function get_data();
-      begin
-	 return myline.data;
-      end
+   function automatic BYTE [L2_LINE_SZ-1:0] get_data();
+      return this.myline.data;
    endfunction // get_data
    
 endclass // L2CLINE
