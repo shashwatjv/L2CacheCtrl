b6555a7933a460d39212593e5e4ce2583d32eee2
shashwatjv <shashwat@pdx.edu>
2017-03-06 21:39:10 -0800

    add Cache set class file

---
+++ design/L2CSET.sv
@@ -0,0 +1,21 @@
+// -------------------------------------------------------------------------
+// Portland State University
+// Course: ECE585
+// 
+// Project: L2 Cache Controller
+// 
+// Filename: L2CSET.sv
+// 
+// Description: Defines the Single Set class and its methods.
+//                 The single set consists of N cachelines where N 
+//                 corresponds to Set N Associativity
+// -------------------------------------------------------------------------
+
+`include "L2CacheCtrl.pkg"
+
+class L2CSET;
+   // Create a cache line of CLINE type
+   CSET myset;
+
+endclass // L2CSET
+
