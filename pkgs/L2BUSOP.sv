57d26669cd50b90330f805232e26ef54f44bf340
shashwatjv <shashwat@pdx.edu>
2017-03-17 00:12:42 -0700

    Added Line Allocation logic, yet to complete eviction and BusOps

---
+++ pkgs/L2BUSOP.sv
@@ -0,0 +1,11 @@
+// -------------------------------------------------------------------------
+// Portland State University
+// Course: ECE585
+// 
+// Project: L2 Cache Controller
+// 
+// Filename: L2BUSOP.sv
+// 
+// Description: Collection of the Bus Operation functions
+// -------------------------------------------------------------------------
+
